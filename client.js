6f5da0310f0d20e2d192f0fbed1c6ae29f9fb0c9
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-08 11:32:13 -0600

    Completed base mode

--- client.js
+++ client.js
@@ -1,23 +1,21 @@
 // console.log(yourFunction(["ASDF", "BLAH", "Oh! ", "Hi ", "There!"], 3));
 
 var wordArray = ["ASDF", "BLAH", "Oh! ", "Hi ", "There!"];
-// console.log(wordArray.join('!!!'));
+var secondWordArray = ["zombie", "taco", "conphident", "fi", "phi", "fo", "fum"];
 
-var stringResult = ''; // starting with an empty string
-
-// stringResult = 'taco';
-
-// concatenate all of the words
-for (var i = 2; i < wordArray.length; i++) {
-  // console.log('before', stringResult);
-  stringResult = stringResult + wordArray[i]; // Add a word to the stringResult
-  // console.log('after', stringResult);
+function putAllOfTheWordsTogether(stringArray, startingIndex) {
+  var stringResult = ''; // starting with an empty string
+  // concatenate all of the words
+  for (var i = startingIndex; i < stringArray.length; i++) {
+    stringResult = stringResult + " " + stringArray[i]; // Add a word to the stringResult
+  }
+  return stringResult;
 }
 
-// stringResult = stringResult + wordArray[0];
-// stringResult = stringResult + wordArray[1];
-// stringResult = stringResult + wordArray[2];
-// stringResult = stringResult + wordArray[3];
-// stringResult = stringResult + wordArray[4];
 
-console.log(stringResult);
+console.log('first time:', putAllOfTheWordsTogether(wordArray, 2));
+console.log('second time:', putAllOfTheWordsTogether(secondWordArray, 2));
+// console.log('second time: ', putAllOfTheWordsTogether(2));
+
+// putAllOfTheWordsTogether(2);
+// console.log('second time: ', stringResult);
